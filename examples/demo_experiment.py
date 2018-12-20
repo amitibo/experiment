0438d20d94951bc0379340cff9766e05e2f08587
Amit Aides <amitaid@il.ibm.com>
2018-12-20 11:42:05 +0200

    Updated example

--- examples/demo_experiment.py
+++ examples/demo_experiment.py
@@ -1,6 +1,6 @@
 from experiment import Experiment
 import logging
-from traitlets import Float, Int
+from traitlets import Enum, Float, Int
 
 
 class Main(Experiment):
@@ -8,13 +8,14 @@ class Main(Experiment):
     # Parameters of experiment
     #
     epochs = Int(10, config=True, help="Number of epochs")
-
     lr = Float(0.1, config=True, help="Learning rate of training")
+    loss = Enum(("mse", "l1"), config=True, default_value="mse", help="Loss type.")
 
     def run(self):
         """Running the experiment"""
 
         logging.info("Starting experiment")
+        logging.info("Using {} loss".format(self.loss))
 
         for i in range(self.epochs):
             logging.info("Epoch: [{}/{}]".format(i, self.epochs))
